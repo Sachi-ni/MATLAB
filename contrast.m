5e11511a957e11dc5a12470c410214d1e71a740f
Sachi-ni <nisansalaweerasingha3848@gmail.com>
2026-01-26 12:06:12 +0530

    Add files via upload

---
+++ contrast.m
@@ -0,0 +1,20 @@
+function I = contrast(I,s)
+if length(size(I)) == 3
+    I=rgb2gray(I);
+end
+[r,c] = size(I);
+for i = 1:r
+    for j = 1:c
+        a = I(i,j);
+        a = a*s;
+        if(a>255)
+            a = 255;
+        end
+        if(a<0)
+            a = 0;
+        end
+        I(i,j) = a;
+    end
+end
+
+        
