ec74537d3748de4a2a82a5de5fc875543fc4658a
Sachi-ni <nisansalaweerasingha3848@gmail.com>
2026-02-06 08:42:16 +0530

    Fix blending order in alphaBlend function

--- alphaBlend.m
+++ alphaBlend.m
@@ -3,12 +3,12 @@ if length(size(IFG)) == 3
   IFG = rgb2gray(IFG);
 end
 if length(size(IBG)) == 3
-  IFG = rgb2gray(IBG);
+  IBG = rgb2gray(IBG);
 end
 [r,c] = size(IBG);
 I = IBG;
 for i = 1:r
   for j =1:c
-    I(i,j) = a*IBG(i,j) + (1-a)*IFG(i,j);
+    I(i,j) = a*IFG(i,j) + (1-a)*IBG(i,j);
   end
 end
