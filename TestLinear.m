d109ed205e37b60a0a09a323c195b678033f97d0
Sachi-ni <nisansalaweerasingha3848@gmail.com>
2026-03-13 08:36:51 +0530

    Change filter to average and add histogram plots
    Updated filter type to average and added histograms.

--- TestLinear.m
+++ TestLinear.m
@@ -1,6 +1,18 @@
 I=imread('lena.jfif');
-F=fspecial('laplacian', 1.0);
+F=fspecial('average', [5,5]);
 J=linearFilter(I,F);
 
-subplot(1,2,1);imshow(rgb2gray(I));hold on;
-subplot(1,2,2);imshow(J);
+h1 = histogram(I);
+h2 = histogram(J);
+subplot(2,2,1);imshow(rgb2gray(I));hold on;
+subplot(2,2,2);imshow(J);hold on;
+subplot(2,2,3);bar(h1);hold on;
+subplot(2,2,4),bar(h2);
+
+
+
+
+% F = fspecial('gaussian', [5,5], 1.0);
+%F = fspecial('laplacian', 0);
+% F = fspecial('average', [5,5]);
+
